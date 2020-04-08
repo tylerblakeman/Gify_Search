66d92eba7b44b0264353a5f2f553544a89e1f4d1
Tyler Blakeman <tylerblakeman@gmail.com>
2020-04-08 18:17:16 -0500

    commiting with enabled https

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,5 @@
 //recommended searches
-var gifSearch = [
-	"wakeboarding",
-	"snowboarding",
-	"mountainbiking",
-	"wake surfing",
-];
+var gifSearch = ["wakeboarding", "snowboarding", "mountainbiking", "wake surfing"];
 var queryResponse;
 var divCount = 0;
 function drawButtons() {
@@ -21,54 +16,49 @@ function drawButtons() {
 
 drawButtons();
 
-$("#add-gif").on("click", function (event) {
-	event.preventDefault();
-	var gif = $("#gif-input").val().trim();
-	$("#gif-input").val("");
-	if (gif == "") {
-		console.log("empty");
-	} else {
-		gifSearch.push(gif);
-		drawButtons();
-	}
+$("#add-gif").on("click", function(event) {
+    event.preventDefault();
+    var gif = $("#gif-input").val().trim();
+    $("#gif-input").val("")
+        if (gif == "") {
+        console.log("empty")
+    }
+    else {
+    gifSearch.push(gif);
+    drawButtons();
+    }
 });
 
 $(document).on("click", ".button", gifSearchStart);
 
 function gifSearchStart() {
-	var gifForURL = $(this).attr("data-name");
+    var gifForURL= $(this).attr("data-name");
+    
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+gifForURL+"&api_key=Tc1MZkuENFcgyA1xoYluJKJcKsIo1XdC&limit=10&rating=pg";
 
-	var queryURL =
-		"https://api.giphy.com/v1/gifs/random?q=" +
-		gifForURL +
-		"&api_key=Tc1MZkuENFcgyA1xoYluJKJcKsIo1XdC&limit=10&rating=pg";
 
-	$.ajax({
-		url: queryURL,
-		method: "GET",
-	})
-	.then(function (response) {
-		console.log(response.data);
-		var imagePush = "";
-		var image = $("<image>");
-		for (let i = 0; i < response.data.length; i++) {
-			var imageURL = response.data[i].images.fixed_width.url;
-			console.log(imageURL);
-			console.log(imageURL);
-			var image = $("<img>");
-			image.attr("src", response.data[i].images.fixed_width.url);
-			var pTag = $("<p>").text(
-				"Rating: " + JSON.stringify(response.data[i].rating)
-			);
-			var div = $(
-				`<div class="imagecontainer` +
-					divCount +
-					` inserted-images col-lg-3 col-md-4 col-6">`
-			);
-			$("#images").prepend(div);
-			$(`.imagecontainer` + divCount).prepend(pTag);
-			$(`.imagecontainer` + divCount).prepend(image);
-			divCount++;
-		}
-	});
-}
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+
+        .then(function(response){
+            console.log(response.data);
+            var imagePush = "";
+            var image = $("<image>")
+            for (let i = 0; i < response.data.length; i++) {
+                var imageURL = response.data[i].images.fixed_width.url;          
+                console.log(imageURL)
+                console.log(imageURL);
+                var image = $("<img>");
+                image.attr("src", response.data[i].images.fixed_width.url);
+                var pTag = $("<p>").text("Rating: "+response.data[i].rating);
+                var div = $(`<div class="imagecontainer`+divCount+` inserted-images col-lg-3 col-md-4 col-6">`)
+                $("#images").prepend(div);
+                $(`.imagecontainer`+divCount).prepend(pTag);
+                $(`.imagecontainer`+divCount).prepend(image);
+                divCount++
+                }
+            });   
+};
